1b91acba387d9245209d421b635a8617d03dde06
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-06-28 17:55:31 +0200

    [project setup] re-created project with libgdx project setup tool

---
+++ desktop/src/com/jafleck/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jafleck.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jafleck.game.PhysicsPuzzlePlatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PhysicsPuzzlePlatformer(), config);
+	}
+}
