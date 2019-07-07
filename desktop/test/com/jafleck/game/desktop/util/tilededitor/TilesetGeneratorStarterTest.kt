2e3ff20c27a9b11bb85998bc971c6f06134a7b12
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-07-07 13:17:40 +0200

    [Use Map Editor #12] generating atlas-tileset via manual unit test -> created separate issue #20 for creating clean plugin to generate tileset

--- desktop/test/com/jafleck/game/desktop/util/tilededitor/TilesetGeneratorStarterTest.kt
+++ desktop/test/com/jafleck/game/desktop/util/tilededitor/TilesetGeneratorStarterTest.kt
@@ -12,11 +12,12 @@ internal class TilesetGeneratorStarterTest {
     @Disabled("manual test - takes too long and also depends on some project files")
     @Test
     fun generateFromAtlas() {
-        val targetPngImageFile = File.createTempFile("atlas-terrain", ".png")
-        val targetTilesetXmlFile = File.createTempFile("atlas-terrain", ".tsx")
-        // uncomment if targeting actual files
-//        val targetPngImageFile = File("../android/assets/maps/atlas-tileset.png")
-//        val targetTilesetXmlFile = File("../android/assets/maps/atlas-tileset.tsx")
+        val targetPngImageFile = File("../android/assets/maps/atlas-tileset.png")
+        val targetTilesetXmlFile = File("../android/assets/maps/atlas-tileset.tsx")
+
+        // uncomment for targeting temporary files
+//        val targetPngImageFile = File.createTempFile("atlas-terrain", ".png")
+//        val targetTilesetXmlFile = File.createTempFile("atlas-terrain", ".tsx")
 
         val atlas = AssetDescriptor("../android/assets/atlas/textures.atlas", TextureAtlas::class.java)
 
