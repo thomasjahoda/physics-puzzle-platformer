fdcbf2c1019f24b76ff2371788a77e6fff40110b
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-07-09 19:11:28 +0200

    [Render entities by filling it drawing a line on the edges and filling #41] create CircleShapeComponent for thrown ball

---
+++ core/src/main/kotlin/com/jafleck/game/components/shape/CircleShapeComponent.kt
@@ -0,0 +1,11 @@
+package com.jafleck.game.components.shape
+
+import com.badlogic.ashley.core.Component
+import com.jafleck.extensions.libgdxktx.ashley.ComponentMapperAccessor
+
+class CircleShapeComponent(
+    val radius: Float
+) : Component, ShapeComponent {
+
+    companion object : ComponentMapperAccessor<CircleShapeComponent>(CircleShapeComponent::class)
+}
--- core/src/main/kotlin/com/jafleck/game/entities/ThrownBallEntity.kt
+++ core/src/main/kotlin/com/jafleck/game/entities/ThrownBallEntity.kt
@@ -14,16 +14,16 @@ import com.jafleck.game.assets.Assets
 import com.jafleck.game.assets.ScreenToWorldScalingPropagator
 import com.jafleck.game.assets.autoScaleByExpectedWorldSize
 import com.jafleck.game.components.*
+import com.jafleck.game.components.shape.CircleShapeComponent
 import com.jafleck.game.components.shape.RectangleShapeComponent
-import com.jafleck.game.families.DrawableRectangle
-import com.jafleck.game.families.MovingBody
 import ktx.box2d.body
 import org.koin.dsl.module
 
 inline class ThrownBallEntity(val entity: Entity) {
 
     companion object {
-        val SIZE = Vector2(0.2f, 0.2f)
+        val RADIUS: Float = 0.1f
+        val SIZE = Vector2(2 * RADIUS, 2 * RADIUS)
         val HALF_SIZE: Vector2 = SIZE.cpy().scl(0.5f)
         const val DENSITY = 10f
         const val FRICTION = 0.2f
@@ -65,13 +65,13 @@ class ThrownBallEntityCreator(
     ): ThrownBallEntity {
         val entity = engine.createEntity().apply {
             add(OriginPositionComponent(originPosition))
-            add(RectangleShapeComponent(ThrownBallEntity.SIZE))
+            add(CircleShapeComponent(ThrownBallEntity.RADIUS))
             add(RectangleBoundsComponent(ThrownBallEntity.SIZE))
             add(DrawableVisualComponent(drawable))
             add(VelocityComponent(velocity))
             add(BodyComponent(world.body {
                 type = BodyDef.BodyType.DynamicBody
-                circle(radius = ThrownBallEntity.HALF_SIZE.x) {
+                circle(radius = ThrownBallEntity.RADIUS) {
                     density = ThrownBallEntity.DENSITY
                     friction = ThrownBallEntity.FRICTION
                     restitution = 0.5f
