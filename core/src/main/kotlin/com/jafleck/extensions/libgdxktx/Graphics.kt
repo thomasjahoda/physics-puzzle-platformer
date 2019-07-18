72cb7508a761c615fef358d89148f014b3de4117
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-07-19 00:27:27 +0200

    [general] do not create or clear depth buffer because z-order rendering is currently not necessary. I might use it in the future if I don't wanna spend too much time on optimization

--- core/src/main/kotlin/com/jafleck/extensions/libgdxktx/Graphics.kt
+++ core/src/main/kotlin/com/jafleck/extensions/libgdxktx/Graphics.kt
@@ -7,5 +7,7 @@ import com.badlogic.gdx.graphics.GL20
 fun clearScreen(color: Color) {
     // see https://stackoverflow.com/questions/35969253/libgdx-antialiasing for antialising, I hope I am doing this right
     ktx.app.clearScreen(color.r, color.g, color.b, 1f)
-    Gdx.gl.glClear(GL20.GL_DEPTH_BUFFER_BIT or (if (Gdx.graphics.bufferFormat.coverageSampling) GL20.GL_COVERAGE_BUFFER_BIT_NV else 0))
+    val clearMask = if (Gdx.graphics.bufferFormat.coverageSampling) GL20.GL_COVERAGE_BUFFER_BIT_NV else 0
+//    clearMask = clearMask or GL20.GL_DEPTH_BUFFER_BIT // uncomment for z-ordering if necessary
+    Gdx.gl.glClear(clearMask)
 }
