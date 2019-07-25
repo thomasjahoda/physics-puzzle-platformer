2352fc45b23857d6c79ce8c9dae8859ca66391b1
sebastian.flecker <flecker@student.tugraz.at>
2019-07-25 14:55:45 +0200

    [general] comment

--- core/src/main/kotlin/com/jafleck/game/entities/physics/CollisionEntityCategory.kt
+++ core/src/main/kotlin/com/jafleck/game/entities/physics/CollisionEntityCategory.kt
@@ -2,6 +2,10 @@ package com.jafleck.game.entities.physics
 
 object CollisionEntityCategory {
     const val none: Short = 0
+    /**
+     * Use if the entity itself defines with whom it wants to collide with and it usually is not referenced
+     * by other entities for collision filtering.
+     */
     const val default: Short = 1
     const val environment: Short = 2
     const val player: Short = 4
